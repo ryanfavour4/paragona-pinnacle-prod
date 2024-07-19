5b9c20d9debe63916646e06e3c07ff329876c5ac
ryanfavour4 <ryanfave022@gmail.com>
2024-07-19 02:28:24 +0100

    loan application complete

--- js/loan-application.js
+++ js/loan-application.js
@@ -1,17 +1,35 @@
 const formLoan = document.querySelector(".form");
+const alertDiv = document.querySelector(".alert-message");
 
 formLoan.addEventListener("submit", (e) => {
   e.preventDefault();
   const formData = new FormData(formLoan);
   const data = Object.fromEntries(formData);
-  console.log(data);
-  fetch("https://paragona-pinnacle-api.onrender.com/api/v1/auth", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-  })
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+  // disable buttons
+  const submitButton = document.querySelector(".btn");
+  submitButton.disabled = true;
+
+  try {
+    // axios.post("http://localhost:8181/api/v1/auth", data)
+    axios.post("https://paragona-pinnacle-api.onrender.com/api/v1/auth", data)
+      .then(res => {
+        console.log(res);
+        alertDiv.innerHTML = `<div class="alert alert-success text-center" role="alert">${res.data.message}</div>`;
+        submitButton.disabled = false;
+        window.location.href = res.data.redirectUrl;
+      })
+      .catch(err => {
+        console.log(err);
+        if (err.response.data) {
+          alertDiv.innerHTML = `<div class="alert alert-danger text-center" role="alert">${err.response.data.message}</div>`;
+        }
+        submitButton.disabled = false;
+        setTimeout(() => {
+          alertDiv.innerHTML = "";
+        }, 10000);
+      })
+  } catch (error) {
+    console.log(error);
+  }
+
 });
