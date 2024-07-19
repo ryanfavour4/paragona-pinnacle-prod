1708de2fce1cbf3008f88b9354daa9fd5598f77c
ryanfavour4 <ryanfave022@gmail.com>
2024-07-19 11:07:52 +0100

    loan application complete

--- js/loan-application.js
+++ js/loan-application.js
@@ -11,25 +11,25 @@ formLoan.addEventListener("submit", (e) => {
 
   try {
     // axios.post("http://localhost:8181/api/v1/auth", data)
-    axios.post("https://paragona-pinnacle-api.onrender.com/api/v1/auth", data)
-      .then(res => {
-        console.log(res);
+    axios
+      .post(
+        "https://paragona-pinnacle-api.onrender.com/api/v1/auth",
+        data
+      )
+      .then((res) => {
         alertDiv.innerHTML = `<div class="alert alert-success text-center" role="alert">${res.data.message}</div>`;
         submitButton.disabled = false;
         window.location.href = res.data.redirectUrl;
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
-        if (err.response.data) {
+        if (err.response.data)
           alertDiv.innerHTML = `<div class="alert alert-danger text-center" role="alert">${err.response.data.message}</div>`;
-        }
         submitButton.disabled = false;
-        setTimeout(() => {
-          alertDiv.innerHTML = "";
-        }, 10000);
-      })
+        setTimeout(() => (alertDiv.innerHTML = ""), 10000);
+      });
   } catch (error) {
     console.log(error);
+    alertDiv.innerHTML = `<div class="alert alert-danger text-center" role="alert">An error occurred. Please try again later.</div>`;
   }
-
 });
